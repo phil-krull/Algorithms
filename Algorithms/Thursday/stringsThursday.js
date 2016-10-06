aed3586c879d9504ddb010e6c366c01c2978d915
phil krull <philip.krull@hotmail.com>
2016-10-06 16:26:33 -0700

    Thursday's JS object demo

---
+++ Algorithms/Thursday/stringsThursday.js
@@ -0,0 +1,12 @@
+// Create an object
+var myObject = {"first_name": "Phil", "last_name": "Krull", "email": "pkrull@codingdojo.com", "age": 33};
+
+// Use a for in loop to interate through the object
+for (key in myObject){
+  // Print the key with the value
+  console.log(`The key is ${key} and the value is ${myObject[key]}`);
+}
+
+// Given an array, return an object containing the array's min, max and average value.
+
+// Given a number of U.S. cent, return an object containing the optimal configuration of coins.  
